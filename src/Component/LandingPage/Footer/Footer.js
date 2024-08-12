8647fb408bc86d8119010a8988cbb31fee049e9d
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-12 09:20:49 +0100

    chore: Add Footer component to LandingPage

---
+++ src/Component/LandingPage/Footer/Footer.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="w-full h-[959px] flex justify-center">
+      <div className="w-[1440px] h-full flex items-center flex-col">
+        <div className="w-[1182px] h-[378px] flex">
+          <div>
+            <div>Moler Health</div>
+          </div>
+          <div className="flex">
+            <div>
+              <div>Company</div>
+              <ul>
+                <li>Features</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Terms of Service</li>
+                <div>User Agreement</div>
+                <div>Community</div>
+                <div>Guidelines</div>
+              </ul>
+            </div>
+            <div>
+              <div>Community & Support</div>
+              <ul>
+                <li>Get Involved</li>
+                <li>Documentation</li>
+                <li>Contact Us</li>
+                <li>Support</li>
+              </ul>
+            </div>
+            <div>
+              <div>Resources</div>
+              <ul>
+                <li>Blog</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>Join Our Newsletter</div>
+        </div>
+        <div>
+          <imput></imput>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
