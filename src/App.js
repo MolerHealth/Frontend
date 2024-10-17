5c86cf99f842713d9b585c58a70b471d751c6922
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-10-17 18:06:26 +0100

    feat: Add new components to App.js
    Add LoginPage, SignUpPage, and DashBoardContent components to the App.js file to enable routing and display the respective pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Component/LandingPage/LandingPage";
+import LoginPage from "./Component/OnboardingPage/LoginPage";
+import SignUpPage from "./Component/OnboardingPage/SignupPage";
+import { DashBoardContent } from "./Component/Dashboard/DashBoardContent";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/dashboard/*" element={<DashBoardContent />} />
       </Routes>
     </Router>
   );
