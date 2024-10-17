3cece1682124811190b7a1abb3cc2efb263c4854
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-10-17 18:08:19 +0100

    feat: Add Void component for dashboard layout

---
+++ src/Component/Dashboard/Void/Void.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Void() {
+  return (
+    <div className="shrink-0 w-[16.25rem] h-full ">
+      <div className="w-full h-full flex items-center justify-center text-white"></div>
+    </div>
+  );
+}
+
+export default Void;
