f3047df9fb5a4dbadb658226e29487ddb60f1796
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-10-17 18:06:12 +0100

    chore: Update tailwind.config.js with new boxShadow and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
         mona: ["Mona-sans", "sans-serif"],
         Niramala: ["Niramala", "sans-serif"],
       },
+      boxShadow: {
+        custom1: "0px 1px 19.2px 0px rgba(0, 0, 0, 0.13)",
+      },
+      colors: {
+        backgroundGrey: "rgba(65, 105, 225, 0.04)",
+        highlightWhite: "rgba(255, 255, 255, 1)",
+      },
     },
   },
   plugins: [],
