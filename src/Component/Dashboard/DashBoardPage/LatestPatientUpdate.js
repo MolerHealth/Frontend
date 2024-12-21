3d61524fcd7732b537f8c919c1f0978736a18b13
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-12-21 22:19:52 +0100

    feat: Enhance Latest Patient Update component with additional patient details and edit functionality

--- src/Component/Dashboard/DashBoardPage/LatestPatientUpdate.js
+++ src/Component/Dashboard/DashBoardPage/LatestPatientUpdate.js
@@ -1,59 +1,101 @@
 import React, { Component } from "react";
 import DotMenuSVG from "../../../Assets/SVG/DotsMenuSVG.svg";
+import EditSVG from "../../../Assets/SVG/EditSVG.svg";
 
 class LatestPatientUpdate extends Component {
   render() {
     return (
-      <div className="flex w-[36.688rem] h-[18.25rem] bg-highlightWhite justify-center">
-        <div className="flex flex-col py-[3rem] gap-[1rem]">
+      <div className="flex w-full px-[1rem] h-fit bg-highlightWhite justify-center">
+        <div className="flex w-full flex-col py-[3rem] gap-[1rem]">
           <div className="font-[600] text-[#333333] text-[1rem] font-mona leading-[1.32rem]">
-            New Patient Appointment
+            Latest Patient Update
           </div>
-          <div>
-            <div className="grid grid-cols-4 gap-[2rem] font-mona font-[500] text-[#4169E1] border-b h-[1.9rem]">
-              <div>Patient ID</div>
-              <div>Name</div>
-              <div>Date</div>
-              <div>Action</div>
+          <div className="flex flex-col h-fit">
+            <div className="grid grid-cols-7 gap-[1rem] font-mona font-[500] text-[#4169E1] border-b h-[1.9rem]">
+              <div className="w-[1rem]">ID</div>
+              <div className="w-[4rem] relative left-[-2rem]">Date In</div>
+              <div className="w-[7rem] relative left-[-3rem]">Name</div>
+              <div className="w-[2.5rem] relative left-[-1rem]">Age</div>
+              <div className="relative left-[-2rem]">Country</div>
+              <div className="relative left-[-1rem]">Gender</div>
+              <div>Modify</div>
             </div>
-            <div className="grid grid-cols-4 gap-[2rem] h-[3.75rem] items-center  border-b">
-              <div>888DD</div>
-              <div>Jessica</div>
-              <div>10/04/24</div>
-              <div>
-                <img src={DotMenuSVG} alt="DotMenu" />
+            <div className="grid grid-cols-7 gap-[1rem] h-[3.75rem] items-center justify-center  border-b">
+              <div className="w-[3rem]">888DD</div>
+              <div className="w-[4rem] relative left-[-2rem]">10/04/24</div>
+              <div className="w-[7rem] relative left-[-3rem]">
+                James Mitchel
+              </div>
+              <div className="w-[2.5rem] relative left-[-1rem]">42yrs</div>
+              <div className="relative left-[-2rem]">USA</div>
+              <div className="relative left-[-1rem]">Male</div>
+              <div className="flex size-[1rem] items-center justify-center relative left-[1rem]">
+                <img src={EditSVG} alt="Edit" />
               </div>
             </div>
-            <div className="grid grid-cols-4 gap-[2rem] h-[3.75rem] items-center  border-b">
-              <div>888DD</div>
-              <div>Jessica</div>
-              <div>10/04/24</div>
-              <div>
-                <img src={DotMenuSVG} alt="DotMenu" />
+            <div className="grid grid-cols-7 gap-[1rem] h-[3.75rem] items-center justify-center  border-b">
+              <div className="w-[3rem]">888DD</div>
+              <div className="w-[4rem] relative left-[-2rem]">10/04/24</div>
+              <div className="w-[7rem] relative left-[-3rem]">
+                James Mitchel
+              </div>
+              <div className="w-[2.5rem] relative left-[-1rem]">42yrs</div>
+              <div className="relative left-[-2rem]">USA</div>
+              <div className="relative left-[-1rem]">Male</div>
+              <div className="flex size-[1rem] items-center justify-center relative left-[1rem]">
+                <img src={EditSVG} alt="Edit" />
               </div>
             </div>
-            <div className="grid grid-cols-4 gap-[2rem] h-[3.75rem] items-center  border-b">
-              <div>888DD</div>
-              <div>Jessica</div>
-              <div>10/04/24</div>
-              <div>
-                <img src={DotMenuSVG} alt="DotMenu" />
+            <div className="grid grid-cols-7 gap-[1rem] h-[3.75rem] items-center justify-center  border-b">
+              <div className="w-[3rem]">888DD</div>
+              <div className="w-[4rem] relative left-[-2rem]">10/04/24</div>
+              <div className="w-[7rem] relative left-[-3rem]">
+                James Mitchel
+              </div>
+              <div className="w-[2.5rem] relative left-[-1rem]">42yrs</div>
+              <div className="relative left-[-2rem]">USA</div>
+              <div className="relative left-[-1rem]">Male</div>
+              <div className="flex size-[1rem] items-center justify-center relative left-[1rem]">
+                <img src={EditSVG} alt="Edit" />
               </div>
             </div>
-            <div className="grid grid-cols-4 gap-[2rem] h-[3.75rem] items-center  border-b">
-              <div>888DD</div>
-              <div>Jessica</div>
-              <div>10/04/24</div>
-              <div>
-                <img src={DotMenuSVG} alt="DotMenu" />
+            <div className="grid grid-cols-7 gap-[1rem] h-[3.75rem] items-center justify-center  border-b">
+              <div className="w-[3rem]">888DD</div>
+              <div className="w-[4rem] relative left-[-2rem]">10/04/24</div>
+              <div className="w-[7rem] relative left-[-3rem]">
+                James Mitchel
+              </div>
+              <div className="w-[2.5rem] relative left-[-1rem]">42yrs</div>
+              <div className="relative left-[-2rem]">USA</div>
+              <div className="relative left-[-1rem]">Male</div>
+              <div className="flex size-[1rem] items-center justify-center relative left-[1rem]">
+                <img src={EditSVG} alt="Edit" />
               </div>
             </div>
-            <div className="grid grid-cols-4 gap-[2rem] h-[3.75rem] items-center  border-b">
-              <div>888DD</div>
-              <div>Jessica</div>
-              <div>10/04/24</div>
-              <div>
-                <img src={DotMenuSVG} alt="DotMenu" />
+            <div className="grid grid-cols-7 gap-[1rem] h-[3.75rem] items-center justify-center  border-b">
+              <div className="w-[3rem]">888DD</div>
+              <div className="w-[4rem] relative left-[-2rem]">10/04/24</div>
+              <div className="w-[7rem] relative left-[-3rem]">
+                James Mitchel
+              </div>
+              <div className="w-[2.5rem] relative left-[-1rem]">42yrs</div>
+              <div className="relative left-[-2rem]">USA</div>
+              <div className="relative left-[-1rem]">Male</div>
+              <div className="flex size-[1rem] items-center justify-center relative left-[1rem]">
+                <img src={EditSVG} alt="Edit" />
+              </div>
+            </div>
+            <div className="grid grid-cols-7 gap-[1rem] h-[3.75rem] items-center justify-center  border-b">
+              <div className="w-[3rem]">888DD</div>
+              <div className="w-[4rem] relative left-[-2rem]">10/04/24</div>
+              <div className="w-[7rem] relative left-[-3rem]">
+                James Mitchel
+              </div>
+              <div className="w-[2.5rem] relative left-[-1rem]">42yrs</div>
+              <div className="relative left-[-2rem]">USA</div>
+              <div className="relative left-[-1rem]">Male</div>
+              <div className="flex size-[1rem] items-center justify-center relative left-[1rem]">
+                <img src={EditSVG} alt="Edit" />
               </div>
             </div>
           </div>
