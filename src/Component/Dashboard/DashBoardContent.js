fd06eb61459bad33f1be89fb670b70329686df95
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-12-21 22:19:39 +0100

    feat: Add patient and allergies routes to dashboard

--- src/Component/Dashboard/DashBoardContent.js
+++ src/Component/Dashboard/DashBoardContent.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import SideNavBar from "./Navbars/SideNavBar/SideNavBar";
 import DashBoardPage from "./DashBoardPage/DashBoardPage";
 import Void from "./Void/Void";
+import DoctorPatientsViewHome from "./DoctorPatientsView/DoctorPatientsViewHome";
+import AllergiesNew from "./DoctorPatientsView/HealthRecord/Allergies/AllergiesNew";
 
 export function DashBoardContent() {
   return (
@@ -15,6 +17,34 @@ export function DashBoardContent() {
               <Void />
               <div className="relative flex w-full h-full">
                 <DashBoardPage />
+              </div>
+              <SideNavBar />
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="/patients"
+        element={
+          <div>
+            <div className="flex min-w-screen min-h-screen bg-backgroundGrey overflow-x-scroll ">
+              <Void />
+              <div className="relative flex w-full h-full">
+                <DoctorPatientsViewHome />
+              </div>
+              <SideNavBar />
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="/patients/allergies"
+        element={
+          <div>
+            <div className="flex min-w-screen min-h-screen bg-backgroundGrey overflow-x-scroll ">
+              <Void />
+              <div className="relative flex w-full h-full">
+                <AllergiesNew />
               </div>
               <SideNavBar />
             </div>
