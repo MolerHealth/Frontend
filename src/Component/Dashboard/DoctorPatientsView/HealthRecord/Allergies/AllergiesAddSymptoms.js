4b620ab7a3de2d6cee3d8e470b802413f01c9408
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-12-23 21:50:22 +0100

    feat: Enhance AllergiesAddSymptoms component layout and styling for better UI presentation

--- src/Component/Dashboard/DoctorPatientsView/HealthRecord/Allergies/AllergiesAddSymptoms.js
+++ src/Component/Dashboard/DoctorPatientsView/HealthRecord/Allergies/AllergiesAddSymptoms.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class AllergiesAddSymptoms extends Component {
   render() {
     return (
-      <div className="py-[1rem] px-[2rem]">
+      <div className="h-full py-[1rem] px-[2rem] overflow-auto ">
         <div className="flex flex-col gap-[0.1rem]">
           <div className="flex gap-[1rem] items-center">
             <div className="font-[600] font-manrope text-[2rem] text-[#333333]">
@@ -16,13 +16,54 @@ class AllergiesAddSymptoms extends Component {
           <div className="font-[600] font-manrope text-[#667085] text-[1.25rem]">
             Reactions
           </div>
-          <div>
-            <div>
-              <div className="">
-                <div></div>
-                <div></div>
+          <div className="flex flex-col gap-[2rem] w-full h-auto py-[1rem]">
+            <div className="flex justify-between w-full">
+              <div className="flex w-[45%] flex-col gap-[1rem] font-[400] font-inter text-[1.5rem]">
+                <div className="flex w-full justify-between">
+                  <div>Hives</div>
+                  <div className="size-[1.5rem] border"></div>
+                </div>
+                <div className="border border-b-[1px] "></div>
               </div>
-              <div></div>
+              <div className="flex w-[45%] flex-col gap-[1rem] font-[400] font-inter text-[1.5rem]">
+                <div className="flex w-full justify-between">
+                  <div>Hives</div>
+                  <div className="size-[1.5rem] border"></div>
+                </div>
+                <div className="border border-b-[1px] "></div>
+              </div>
+            </div>
+            <div className="flex justify-between w-full">
+              <div className="flex w-[45%] flex-col gap-[1rem] font-[400] font-inter text-[1.5rem]">
+                <div className="flex w-full justify-between">
+                  <div>Hives</div>
+                  <div className="size-[1.5rem] border"></div>
+                </div>
+                <div className="border border-b-[1px] "></div>
+              </div>
+              <div className="flex w-[45%] flex-col gap-[1rem] font-[400] font-inter text-[1.5rem]">
+                <div className="flex w-full justify-between">
+                  <div>Hives</div>
+                  <div className="size-[1.5rem] border"></div>
+                </div>
+                <div className="border border-b-[1px] "></div>
+              </div>
+            </div>
+            <div className="flex justify-between w-full">
+              <div className="flex w-[45%] flex-col gap-[1rem] font-[400] font-inter text-[1.5rem]">
+                <div className="flex w-full justify-between">
+                  <div>Hives</div>
+                  <div className="size-[1.5rem] border"></div>
+                </div>
+                <div className="border border-b-[1px] "></div>
+              </div>
+              <div className="flex w-[45%] flex-col gap-[1rem] font-[400] font-inter text-[1.5rem]">
+                <div className="flex w-full justify-between">
+                  <div>Hives</div>
+                  <div className="size-[1.5rem] border"></div>
+                </div>
+                <div className="border border-b-[1px] "></div>
+              </div>
             </div>
           </div>
         </div>
