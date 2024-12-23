78cebf6e7dd3c72899a0583f33b6ac93dd85b250
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-12-23 21:50:01 +0100

    feat: Add new routes for medication, medical condition, family history, and medical documents in Dashboard

--- src/Component/Dashboard/DashBoardContent.js
+++ src/Component/Dashboard/DashBoardContent.js
@@ -5,6 +5,10 @@ import DashBoardPage from "./DashBoardPage/DashBoardPage";
 import Void from "./Void/Void";
 import DoctorPatientsViewHome from "./DoctorPatientsView/DoctorPatientsViewHome";
 import AllergiesNew from "./DoctorPatientsView/HealthRecord/Allergies/AllergiesNew";
+import MedicationNew from "./DoctorPatientsView/HealthRecord/Medication/MedicationNew";
+import MedicalConditionNew from "./DoctorPatientsView/HealthRecord/MedicalCondition/MedicalConditionNew";
+import FamilyHistoryNew from "./DoctorPatientsView/HealthRecord/FamilyHistory/FamilyHistoryNew";
+import MedicalDocsNew from "./DoctorPatientsView/HealthRecord/MedicalDocs/MedicalDocsNew";
 
 export function DashBoardContent() {
   return (
@@ -45,6 +49,62 @@ export function DashBoardContent() {
               <Void />
               <div className="relative flex w-full h-full">
                 <AllergiesNew />
+              </div>
+              <SideNavBar />
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="/patients/medication"
+        element={
+          <div>
+            <div className="flex min-w-screen min-h-screen bg-backgroundGrey overflow-x-scroll ">
+              <Void />
+              <div className="relative flex w-full h-full">
+                <MedicationNew />
+              </div>
+              <SideNavBar />
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="/patients/medical-condition"
+        element={
+          <div>
+            <div className="flex min-w-screen min-h-screen bg-backgroundGrey overflow-x-scroll ">
+              <Void />
+              <div className="relative flex w-full h-full">
+                <MedicalConditionNew />
+              </div>
+              <SideNavBar />
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="/patients/family-history"
+        element={
+          <div>
+            <div className="flex min-w-screen min-h-screen bg-backgroundGrey overflow-x-scroll ">
+              <Void />
+              <div className="relative flex w-full h-full">
+                <FamilyHistoryNew />
+              </div>
+              <SideNavBar />
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="/patients/medical-docs"
+        element={
+          <div>
+            <div className="flex min-w-screen min-h-screen bg-backgroundGrey overflow-x-scroll ">
+              <Void />
+              <div className="relative flex w-full h-full">
+                <MedicalDocsNew />
               </div>
               <SideNavBar />
             </div>
