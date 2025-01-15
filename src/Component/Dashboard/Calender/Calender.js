ec27d18c2a0e96e4c53485e1304acd36a133217e
mqnifestkelvin <mqnifestkelvin@gmail.com>
2025-01-15 19:48:01 +0100

    feat: update calendar component layout and styling for improved responsiveness

--- src/Component/Dashboard/Calender/Calender.js
+++ src/Component/Dashboard/Calender/Calender.js
@@ -6,20 +6,20 @@ const CustomCalendar = () => {
   const [value, setValue] = useState(new Date());
 
   return (
-    <div className="flex bg-highlightWhite w-[30rem] h-[30rem]">
-      <div className="w-full h-full p-4 bg-highlightWhite rounded-lg shadow-none border-none">
+    <div className="flex bg-highlightWhite w-[32rem] h-auto min-h-[32rem]">
+      <div className="w-full h-full bg-highlightWhite rounded-lg shadow-none border-none  ">
         <Calendar
           onChange={setValue}
           value={value}
-          className="w-full h-full !border-none"
+          className="!w-full !h-full !border-none flex flex-col justify-center items-center"
           next2Label={null}
           prev2Label={null}
           tileClassName={({ date, view }) => {
             const isToday = date.toDateString() === new Date().toDateString();
 
             return `w-12 h-12 flex items-center justify-center text-center
-              hover:bg-blue-100 hover:rounded-full
-              ${isToday ? "bg-blue-700 text-white rounded-full" : ""}`; // Ensure circular shape
+               hover:rounded-full
+              ${isToday ? "bg-blue-700 text-white rounded-full" : ""}`;
           }}
         />
       </div>
