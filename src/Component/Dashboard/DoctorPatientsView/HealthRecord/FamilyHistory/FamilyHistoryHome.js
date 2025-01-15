ed748323f5f001205f48c53c2c7fa54bd603db6a
mqnifestkelvin <mqnifestkelvin@gmail.com>
2025-01-15 13:02:36 +0100

    feat: add navigation link to family history section in FamilyHistoryHome component

--- src/Component/Dashboard/DoctorPatientsView/HealthRecord/FamilyHistory/FamilyHistoryHome.js
+++ src/Component/Dashboard/DoctorPatientsView/HealthRecord/FamilyHistory/FamilyHistoryHome.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import AddBotton from "../../../../../Assets/SVG/AddBotton.svg";
 import TrashIcon from "../../../../../Assets/SVG/TrashIcon.svg";
 import FamilyHistory from "../../../../../Assets/SVG/FamilyHistoryIcon.svg";
+import { Link } from "react-router-dom";
 
 class FamilyHistoryHome extends Component {
   render() {
@@ -19,16 +20,18 @@ class FamilyHistoryHome extends Component {
                 Family History
               </div>
             </div>
-            <div className="flex gap-[1rem] items-center">
-              <div className="font-[400] font-manrope text-[1.25rem] text-[#4169E1]">
-                ADD
+            <Link to="/dashboard/patients/family-history">
+              <div className="flex gap-[1rem] items-center">
+                <div className="font-[400] font-manrope text-[1.25rem] text-[#4169E1]">
+                  ADD
+                </div>
+                <img
+                  className="size-[1.25rem]"
+                  src={AddBotton}
+                  alt="Allergy Icon"
+                />
               </div>
-              <img
-                className="size-[1.25rem]"
-                src={AddBotton}
-                alt="Allergy Icon"
-              />
-            </div>
+            </Link>
           </div>
           <div className="px-[1rem] flex flex-col gap-[1rem]">
             <div className="flex items-center justify-between">
